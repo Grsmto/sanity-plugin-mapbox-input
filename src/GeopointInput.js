e2961d2436c76fc39de62cf1a38693ce59b0a2f4
Adrien Denat <adrien.denat@gmail.com>
2019-08-13 19:44:37 -0300

    first release

---
+++ src/GeopointInput.js
@@ -0,0 +1,182 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { StaticMap, Marker } from 'react-map-gl'
+import config from 'config:mapbox-input'
+import Button from 'part:@sanity/components/buttons/default'
+import Dialog from 'part:@sanity/components/dialogs/default'
+import Fieldset from 'part:@sanity/components/fieldsets/default'
+import {
+  PatchEvent,
+  set,
+  setIfMissing,
+  unset,
+} from 'part:@sanity/form-builder/patch-event'
+import styles from '../styles/GeopointInput.css'
+import GeopointSelect from './GeopointSelect'
+import Pin from './Pin'
+
+const getLocale = context => {
+  const intl = context.intl || {}
+  return (
+    intl.locale ||
+    (typeof window !== 'undefined' && window.navigator.language) ||
+    'en'
+  )
+}
+
+class GeopointInput extends React.Component {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    markers: PropTypes.arrayOf(
+      PropTypes.shape({
+        type: PropTypes.string,
+      })
+    ),
+    value: PropTypes.shape({
+      lat: PropTypes.number,
+      lng: PropTypes.number,
+    }),
+    type: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+    }),
+  }
+
+  static defaultProps = {
+    markers: [],
+  }
+
+  static contextTypes = {
+    intl: PropTypes.shape({
+      locale: PropTypes.string,
+    }),
+  }
+
+  constructor() {
+    super()
+
+    this.handleToggleModal = this.handleToggleModal.bind(this)
+    this.handleCloseModal = this.handleCloseModal.bind(this)
+
+    this.state = {
+      modalOpen: false,
+    }
+  }
+
+  handleToggleModal() {
+    this.setState(prevState => ({ modalOpen: !prevState.modalOpen }))
+  }
+
+  handleChange = lngLat => {
+    const { type, onChange } = this.props
+    onChange(
+      PatchEvent.from([
+        setIfMissing({
+          _type: type.name,
+        }),
+        set(lngLat[1], ['lat']),
+        set(lngLat[0], ['lng']),
+      ])
+    )
+  }
+
+  handleClear = () => {
+    const { onChange } = this.props
+    onChange(PatchEvent.from(unset()))
+  }
+
+  handleCloseModal() {
+    this.setState({ modalOpen: false })
+  }
+
+  render() {
+    const { value, type, markers } = this.props
+
+    if (!config || !config.apiKey) {
+      return (
+        <div>
+          <p>
+            The{' '}
+            <a href="https://sanity.io/docs/schema-types/geopoint-type">
+              Geopoint type
+            </a>{' '}
+            needs a Google Maps API key with access to:
+          </p>
+          <ul>
+            <li>Google Maps JavaScript API</li>
+            <li>Google Places API Web Service</li>
+            <li>Google Static Maps API</li>
+          </ul>
+          <p>
+            Please enter the API key with access to these services in
+            <code style={{ whitespace: 'nowrap' }}>
+              `&lt;project-root&gt;/config/@sanity/google-maps-input.json`
+            </code>
+          </p>
+        </div>
+      )
+    }
+
+    return (
+      <Fieldset
+        legend={type.title}
+        description={type.description}
+        className={styles.root}
+        markers={markers}
+      >
+        {value && (
+          <div>
+            <StaticMap
+              width="100%"
+              height={300}
+              latitude={value.lat}
+              longitude={value.lng}
+              zoom={13}
+              mapboxApiAccessToken={config.apiKey}
+            >
+              <Marker latitude={value.lat} longitude={value.lng}>
+                <Pin />
+              </Marker>
+            </StaticMap>
+          </div>
+        )}
+
+        <div className={styles.functions}>
+          <Button onClick={this.handleToggleModal}>
+            {value ? 'Edit' : 'Set location'}
+          </Button>
+
+          {value && (
+            <Button type="button" onClick={this.handleClear}>
+              Remove
+            </Button>
+          )}
+        </div>
+
+        {this.state.modalOpen && (
+          <Dialog
+            title="Place on map"
+            onClose={this.handleCloseModal}
+            onCloseClick={this.handleCloseModal}
+            onOpen={this.handleOpenModal}
+            message="Select location by dragging the marker or search for a place"
+            isOpen={this.state.modalOpen}
+          >
+            <div className={styles.dialogInner}>
+              <GeopointSelect
+                value={value}
+                apiKey={config.apiKey}
+                onChange={this.handleChange}
+                defaultLocation={config.defaultLocation}
+                defaultZoom={config.defaultZoom}
+                locale={getLocale(this.context)}
+              />
+            </div>
+          </Dialog>
+        )}
+      </Fieldset>
+    )
+  }
+}
+
+export default GeopointInput
---
+++ src/GeopointSelect.js
@@ -0,0 +1,131 @@
+import 'mapbox-gl/dist/mapbox-gl.css?raw'
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css?raw'
+import PropTypes from 'prop-types'
+import React from 'react'
+import ReactMapGL, { Marker } from 'react-map-gl'
+import Geocoder from 'react-map-gl-geocoder'
+import styles from '../styles/GeopointSelect.css'
+import Pin from './Pin'
+
+class GeopointSelect extends React.Component {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.shape({
+      lat: PropTypes.number,
+      lng: PropTypes.number,
+    }),
+    defaultLocation: PropTypes.shape({
+      lat: PropTypes.number,
+      lng: PropTypes.number,
+    }),
+    defaultZoom: PropTypes.number,
+    locale: PropTypes.string,
+  }
+
+  static defaultProps = {
+    defaultZoom: 8,
+    defaultLocation: { lng: 10.74609, lat: 59.91273 },
+  }
+
+  state = {
+    viewport: {
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8,
+    },
+  }
+
+  mapRef = React.createRef()
+
+  constructor(props) {
+    super(props)
+
+    const { value, defaultLocation } = props
+
+    this.state = {
+      viewport: {
+        latitude: this.getValueLatLng().lat,
+        longitude: this.getValueLatLng().lng,
+        zoom: this.props.defaultZoom,
+      },
+    }
+  }
+
+  handleViewportChange = viewport => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport },
+    })
+  }
+
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  handleGeocoderViewportChange = viewport => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 }
+
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides,
+    })
+  }
+
+  getValueLatLng() {
+    const { value } = this.props
+
+    return {
+      lat: value ? value.lat : defaultLocation.lat,
+      lng: value ? value.lng : defaultLocation.lng,
+    }
+  }
+
+  handleOnResult = event => {
+    this.setValue(event.result.geometry.coordinates)
+  }
+
+  handleMarkerDragEnd = event => {
+    this.setValue(event.lngLat)
+  }
+
+  handleMapClick = pointerEvent => {
+    if (pointerEvent.target.classList.contains('overlays')) {
+      this.setValue(pointerEvent.lngLat)
+    }
+  }
+
+  setValue(geoPoint) {
+    this.props.onChange(geoPoint)
+  }
+
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <ReactMapGL
+          width="100%"
+          height="100%"
+          ref={this.mapRef}
+          {...this.state.viewport}
+          onViewportChange={viewport => this.setState({ viewport })}
+          mapboxApiAccessToken={this.props.apiKey}
+          onClick={this.handleMapClick}
+        >
+          <Geocoder
+            mapRef={this.mapRef}
+            onResult={this.handleOnResult}
+            onViewportChange={this.handleGeocoderViewportChange}
+            mapboxApiAccessToken={this.props.apiKey}
+            position="top-left"
+            language={this.props.locale}
+          />
+          <Marker
+            latitude={this.getValueLatLng().lat}
+            longitude={this.getValueLatLng().lng}
+            draggable
+            onDragEnd={this.handleMarkerDragEnd}
+          >
+            <Pin />
+          </Marker>
+        </ReactMapGL>
+      </div>
+    )
+  }
+}
+
+export default GeopointSelect
